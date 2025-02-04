0da7d9782b48039b840870aaca9ab03a6a965984
jimpatton <jimpatton5150@gmail.com>
2025-02-03 20:28:02 -0500

    init commit

---
+++ Temperature Converter/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Temperature_Converter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Temperature Converter");
+            //convert temp from Fahrenheit to Celcius: c= (f-32) * 5/9
+            //continue  "y" or "Y"
+
+            string choice = "y";
+            while (choice == "y" || choice == "Y")
+            {
+                Console.Write("Enter degrees in Fahrenheit: ");
+                double fDegrees = Double.Parse(Console.ReadLine());
+                double cDegrees = ((fDegrees - 32) * 5 / 9);
+                cDegrees = Math.Round(cDegrees,2);
+                Console.WriteLine("Degrees in Celcius: "+ cDegrees);
+                Console.Write("\nContinue? (y or n) ");
+                choice = Console.ReadLine();
+                
+               
+            }
+
+
+
+
+
+
+            Console.WriteLine("Bye");
+        }
+    }
+}
