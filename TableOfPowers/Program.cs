122e22734f07fbbab8e944e6ba5250e69f1158bf
jimpatton <jimpatton5150@gmail.com>
2025-02-05 16:17:46 -0500

    init commit

---
+++ TableOfPowers/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Xml.Serialization;
+
+namespace TableOfPowers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Squares and Cubes Table");
+
+            //valid intergers only
+            //continue with y or Y
+            //enter an integer
+            string choice = "y";
+            while (choice == "y" || choice == "Y")
+            {
+                int nbr = 0;
+                Console.Write("\nEnter an integer: ");
+                nbr = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Number\t\tSquared\t\tCubed");
+                Console.WriteLine("======\t\t=======\t\t=====");
+                //square of an interger is i*i
+                int squared = nbr * nbr;
+                //cube of an interger is i*i*i
+                int cubed = nbr * nbr * nbr;
+                //display Number    Squared     Cubed (double underlined)
+                for (int i = 1; i <= nbr; i++)
+                {
+                    Console.WriteLine(i + "\t\t" + i*i + "\t\t" + i*i*i);
+
+                }
+
+                Console.Write("\nContinue? (y/n): ");
+                choice = (Console.ReadLine());
+            }
+
+
+
+
+
+
+
+            Console.WriteLine("\nBye");
+        }
+    }
+}
