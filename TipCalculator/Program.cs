5610923de49a094535e45d76a9ca686b88f73d98
jimpatton <jimpatton5150@gmail.com>
2025-02-06 14:21:06 -0500

    init commit

---
+++ TipCalculator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Numerics;
+
+namespace TipCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Tip Calculator");
+
+            string choice = "y";
+            while (choice == "y") {
+                Console.Write("Cost of meal: ");
+                // prompt for cost of meal: decimal
+                // skipping validation
+                decimal costOfMeal = Decimal.Parse(Console.ReadLine()); 
+                // biz logic:
+                    //calulate and display tipamt: decimal & totalAmt: decimal   for .15 .20 .25
+                    // for decimal d = .15m; d<+ .25m; d += .05
+                for (decimal d =.15m; d<= .25m; d += .05m)
+                {
+                    decimal tipAmt = costOfMeal * d;
+                    decimal totalAmt = costOfMeal + tipAmt;
+                    Console.WriteLine(d.ToString("p0"));
+                    Console.WriteLine("Tip Amount " + tipAmt.ToString("c"));
+                    Console.WriteLine("Total Amount " +totalAmt.ToString("c"));
+
+                    Console.WriteLine();
+                }
+
+
+
+
+                Console.WriteLine("Continue? (y/n)");
+                Console.ReadLine().ToLower();
+            }
+
+
+
+
+
+
+            Console.WriteLine("Bye");
+        }
+    }
+}
