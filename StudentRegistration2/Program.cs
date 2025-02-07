282751bf77ecf60c4606774421a8a86ced7d8367
jimpatton <jimpatton5150@gmail.com>
2025-02-07 14:36:26 -0500

    init commit

---
+++ StudentRegistration2/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace StudentRegistration2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintLine("Student Registration");
+            // prompt for 1st name,
+            // prompt for last name
+            // prompt for year
+            //return msg "Welcome (first and last name)"
+            //return msg "Your registration is complete"
+            //return msg "Your temporary password is (first + * + year of birth)
+            Print("\nEnter first name: ");
+            string firstName = Console.ReadLine();
+            Print("Enter last name: ");
+            string lastName = Console.ReadLine();
+            Print("Enter year of birth: ");
+            string year = Console.ReadLine();
+            PrintLine("\nWelcome " + firstName + " " + lastName);
+            PrintLine("Your registration is complete.");
+            PrintLine("Your temporary password is: " + firstName + "*" + year);
+
+
+
+            PrintLine("\nBye");
+        }
+        static void PrintLine(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+        static void Print(string msg)
+        {
+            Console.Write(msg);
+        }
+
+    }
+}
