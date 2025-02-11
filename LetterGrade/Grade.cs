b88abff7b741aa4d3afc2f55b056e7b36e41f7f9
jimpatton <jimpatton5150@gmail.com>
2025-02-10 21:24:58 -0500

    init commit

---
+++ LetterGrade/Grade.cs
@@ -0,0 +1,64 @@
+﻿using ContactList;
+
+namespace LetterGrade
+{
+    public class Grade
+    {
+        public string A { get; set; }
+        public string B { get; set; }
+        public string C { get; set; }
+        public string D { get; set; }
+        public string F { get; set; }
+
+        public Grade()
+        {
+        }
+
+        public static void SetNumber(int number)
+        {
+        }
+
+        public static int GetNumber(int prompt, int min, int max)
+        {
+            int result = 0;
+            bool isValid = false;
+            while (!isValid)
+            {
+                Console.Write(prompt);
+                if (Int32.TryParse(Console.ReadLine(), out result))
+                {
+                    if (result >= min && result <= max)
+                    {
+
+                        isValid = true;
+                    }
+                    else
+                    {
+                        MyConsole.PrintLine("Error must be within range");
+                    }
+
+                }
+                else
+                {
+                    MyConsole.PrintLine("Error must be an integer");
+                }
+            }
+        }
+
+        public string GetLetter()
+        {
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ LetterGrade/Program.cs
@@ -0,0 +1,21 @@
+﻿using ContactList;
+
+namespace LetterGrade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Letter Grade Converter!");
+
+
+
+
+
+
+
+            MyConsole.PrintLine("\nBye");
+        }
+
+    }
+}
