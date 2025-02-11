fe75c0822a4d72665f8e1de60f96f2d1706221a8
jimpatton <jimpatton5150@gmail.com>
2025-02-10 19:33:03 -0500

    init commit

---
+++ ContactList/Contact.cs
@@ -0,0 +1,28 @@
+﻿namespace ContactList
+{
+    public class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+        public Contact()
+        { }
+        public Contact(string FirstName, string LastName, string Email, string Phone)
+            {
+                this.FirstName = FirstName;
+                this.LastName = LastName;
+                this.Email = Email;
+                this.Phone = Phone;
+            }
+        public string DisplayContact()
+        {
+            return$"Contact({FirstName}, {LastName}, {Email}, {Phone})";
+        }
+
+
+
+    }
+}
---
+++ ContactList/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Xml.Serialization;
+
+namespace ContactList
+{
+    internal class Program
+    {
+        static List<Contact> Contacts = new List<Contact>();
+        static void Main(string[] args)
+        {
+            MyConsole.PrintLine("Welcome to the Contact List Application!\n");
+            //create class to store information
+
+            Contact newContact = new Contact();
+            string choice = "y";
+            while (choice == "y")
+            {
+                //while loop: continue
+                //prompt user for first name/ last name/ email/ phone
+
+                string FirstName = MyConsole.PromptString("Enter first name: ");
+                string LastName = MyConsole.PromptString("Enter last name: ");
+                string Email = MyConsole.PromptString("Enter email address: ");
+                string Phone = MyConsole.PromptString("Enter phone: ");
+                newContact = new Contact(FirstName,LastName,Email, Phone);
+                Contacts.Add(newContact);
+                
+
+                //display contact information
+                MyConsole.PrintLine("\n-----------------------------------");
+                MyConsole.PrintLine("----Current Contact----------------");
+                MyConsole.PrintLine("-----------------------------------");
+                foreach (Contact contact in Contacts) 
+                {
+                    MyConsole.PrintLine(contact.ToString());
+                }
+                MyConsole.PrintLine("-----------------------------------");
+
+
+                choice = MyConsole.PromptString("\nContinue? (y/n)");
+                
+            }
+
+            MyConsole.PrintLine("Bye");
+        }
+    }
+}
