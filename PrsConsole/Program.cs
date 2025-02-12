03dfaebb8786b9adb8655c0bcd6f126332a1a9ab
jimpatton <jimpatton5150@gmail.com>
2025-02-11 21:05:23 -0500

    init commit

---
+++ PrsConsole/Program.cs
@@ -0,0 +1,47 @@
+﻿using ConsoleLibrary;
+namespace PrsConsole
+{
+    internal class Program
+    {
+        static List<User> Users = new List<User>();
+        static void Main(string[] args)
+        {
+            MyConsole.PrintLine("PrsConsole!");
+            User newUser = new User();
+            newUser.Id = 1;
+            newUser.UserName = "SarlacSlayer";
+            newUser.Password = "BountyHunt";
+            newUser.FirstName = "Boba";
+            newUser.LastName = "Fett";
+            newUser.PhoneNumber = "513-555-5555";
+            newUser.Email = "boba@slave1.com";
+            newUser.Reviewer = "No";
+            newUser.Admin = "No";
+            Users.Add(newUser);
+
+            string choice = "y";
+            while (choice == "y")
+            {
+                int Id = MyConsole.PromptInt("User Id: ");
+                string UserName = MyConsole.PromptString("Username: ");
+                string Password = MyConsole.PromptString("Password: ");
+                string FirstName = MyConsole.PromptString("First Name: ");
+                string LastName = MyConsole.PromptString("Last Name: ");
+                string PhoneNumber = MyConsole.PromptString("Phone number: ");
+                string Email = MyConsole.PromptString("Email: ");
+                string Reviewer = MyConsole.PromptString("Reviewer: ");
+                string Admin = MyConsole.PromptString("Amin: ");
+                newUser = new User(Id, UserName, Password, FirstName, LastName, PhoneNumber, Email, Reviewer, Admin);
+                Users.Add(newUser);
+                choice = MyConsole.PromptString("\nAdd another user? (y/n)");
+
+            }
+            MyConsole.PrintLine("========== Users ==========");
+            foreach (User user in Users) { 
+            MyConsole.PrintLine(user.ToString());
+            }
+
+            MyConsole.PrintLine("\nBye");
+        }
+    }
+}
---
+++ PrsConsole/User.cs
@@ -0,0 +1,45 @@
+﻿
+namespace PrsConsole
+{
+    public class User
+    {
+
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Reviewer { get; set; }
+        public string Admin { get; set; }
+
+        public User()
+        {
+        }
+
+        public User(int Id, string UserName, string Password, string FirstName, string LastName, string PhoneNumber, string Email, string Reviewer, string Admin)
+        {
+            this.Id = Id;
+            this.UserName = UserName;
+            this.Password = Password;
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.PhoneNumber = PhoneNumber;
+            this.Email = Email;
+            this.Reviewer = Reviewer;
+            this.Admin = Admin;
+        }
+
+        public string ToString()
+        {
+            return $"User({Id}: {UserName}, {Password}, {FirstName}, {LastName}, {PhoneNumber}, {Email}, {Reviewer}, {Admin})";
+        }
+
+
+
+
+
+
+    }
+}
