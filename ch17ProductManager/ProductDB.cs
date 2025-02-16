f413b295228d7e418103aed2706f1396306ba7a1
jimpatton <jimpatton5150@gmail.com>
2025-02-16 14:02:44 -0500

    added delete product

---
+++ ch17ProductManager/ProductDB.cs
@@ -0,0 +1,56 @@
+﻿using System.IO.MemoryMappedFiles;
+
+namespace ch17ProductManager
+{
+    internal class ProductDB
+    {
+        private const string Dir = @"c:\C#\Files\";
+        private const string Path = Dir + "Products.txt";
+        private const string Sep = "|";
+
+
+        public static void SaveProducts(List<Product> products)
+        {
+            using StreamWriter writer = new StreamWriter(new FileStream(Path, FileMode.Create, FileAccess.Write));
+
+            foreach (Product product in products)
+            {
+                writer.Write(product.Code + Sep);
+                writer.Write(product.Description + Sep);
+                writer.WriteLine(product.Price);
+            }
+
+
+
+            //writer.Close(); -> eliminated by 'using' statement
+        }
+        public static List<Product> GetProducts()
+        {
+            using StreamReader reader = new StreamReader(new FileStream(Path, FileMode.Open, FileAccess.Read));
+            List<Product> products = new List<Product>();
+            while (reader.Peek() != -1) 
+            {
+                string row = reader.ReadLine();
+                string[] cols = row.Split(Sep);
+
+                if (cols.Length == 3)
+                {
+                    string code = cols[0];
+                    string description = cols[1];
+                    decimal price = Decimal.Parse(cols[2]);
+                    Product p = new Product(code, description, price);
+                    products.Add(p);
+                }
+            }
+            return products;
+
+        }
+
+
+
+
+    }
+
+
+
+}
---
+++ ch17ProductManager/Program.cs
@@ -0,0 +1,121 @@
+﻿using ConsoleLibrary;
+
+namespace ch17ProductManager
+{
+    internal class Program
+    {
+        static List<Product> products = new List<Product>();
+        static void Main(string[] args)
+        {
+            MyConsole.PrintLine("Welcome to product manager!\n");
+
+            //program that provides CRUD features for products
+            //products stored in text file named products.txt
+
+            //load products from file
+            products = ProductDB.GetProducts();
+
+            int command = 9;
+            while (command != 0)
+            {
+                DisplayMenu();
+                command = MyConsole.PromptInt("Command: ");
+                switch (command)
+                {
+                    case 1:
+                        ListProducts();
+                        break;
+                    case 2:
+                        AddProduct();
+                        break;
+                    case 3:
+                        DeleteProduct();
+                        break;
+                    case 0:
+                        break;
+                    default:
+                        MyConsole.PrintLine("Invalid Command. Try again.");
+                        break;
+                }
+            }
+
+
+
+
+
+
+            MyConsole.PrintLine("\nBye");
+        }
+
+        private static void DeleteProduct()
+        {
+            MyConsole.PrintLine("\nDelete Products");
+            MyConsole.PrintLine("===================");
+            string code = MyConsole.PromptString("Product Code to delete: ");
+            //loop through products
+            Product found = null;
+            foreach (Product p in products)
+            {
+                if (p.Code == code)
+                {
+                    found = p;
+                    break;
+                }
+            }
+            if (found != null)
+            {
+                products.Remove(found);
+                ProductDB.SaveProducts(products);
+                MyConsole.PrintLine("Product removed.\n");
+            }
+            else
+            {
+                MyConsole.PrintLine("\nProduct not found");
+            }
+
+            //find product whose code matches code to delete
+            //remove that product from list
+
+
+
+            //save products
+
+
+
+
+        }
+
+        private static void AddProduct()
+        {
+            MyConsole.PrintLine("\nAdd Products");
+            MyConsole.PrintLine("================");
+            string code = MyConsole.PromptString("Code: ");
+            string desc = MyConsole.PromptString("Description: ");
+            decimal price = MyConsole.PromptDecimal("Price: ");
+            Product p = new Product(code, desc, price);
+            products.Add(p);
+            ProductDB.SaveProducts(products);
+            MyConsole.PrintLine("Product added.\n");
+        }
+
+        private static void ListProducts()
+        {
+            MyConsole.PrintLine("\nList Products");
+            MyConsole.PrintLine("=================");
+            //check to see if list is not empty
+            foreach (Product p in products)
+            {
+                MyConsole.PrintLine(p.ToString());
+            }
+        }
+
+        static void DisplayMenu()
+        {
+            MyConsole.PrintLine("\nCOMMAND MENU:");
+            MyConsole.PrintLine("1) List Products");
+            MyConsole.PrintLine("2) Add Product");
+            MyConsole.PrintLine("3) Delete Product");
+            MyConsole.PrintLine("0) Exit");
+        }
+    }
+}
