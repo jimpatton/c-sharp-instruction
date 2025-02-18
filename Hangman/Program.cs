9b63001fc9f31e094f11f74d0fe3c5e1d9b468f2
jimpatton <jimpatton5150@gmail.com>
2025-02-17 20:55:06 -0500

    init commit

---
+++ Hangman/Program.cs
@@ -0,0 +1,25 @@
+﻿using ConsoleLibrary;
+
+namespace Hangman
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyConsole.PrintLine("Welcome to Hangman!\n");
+
+            MyConsole.PrintLine("------");
+            MyConsole.PrintLine("|  |");
+            MyConsole.PrintLine("|  O");
+            MyConsole.PrintLine("| /|\\");
+            MyConsole.PrintLine("| / \\");
+            MyConsole.PrintLine("|");
+            MyConsole.PrintLine("______");
+
+
+
+
+            MyConsole.PrintLine("\nBye");
+        }
+    }
+}
