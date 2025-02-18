33b7b8fe479c1c11e423f1b39cf29754a5cf9616
jimpatton <jimpatton5150@gmail.com>
2025-02-18 14:32:30 -0500

    iteration 2

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -4,22 +4,110 @@ namespace Hangman
 {
     internal class Program
     {
+        static string[] images = new string[7];
         static void Main(string[] args)
+
         {
             MyConsole.PrintLine("Welcome to Hangman!\n");
 
-            MyConsole.PrintLine("------");
-            MyConsole.PrintLine("|  |");
-            MyConsole.PrintLine("|  O");
-            MyConsole.PrintLine("| /|\\");
-            MyConsole.PrintLine("| / \\");
-            MyConsole.PrintLine("|");
-            MyConsole.PrintLine("______");
+            PopulateImages();
+
+            for (int i = 0; i < images.Length; i++)
+            {
+                MyConsole.PrintLine($"Image{i}");
+                MyConsole.PrintLine(images[i]);
+                MyConsole.PrintLine("================");
+
+            }
+
+
 
 
 
 
             MyConsole.PrintLine("\nBye");
         }
+
+        private static string GetHangmanImage(int incorrectGuesses)
+        {
+            return images[incorrectGuesses];
+        }
+        public static void PopulateImages()
+        {
+            images[0] =
+                """
+                -----
+                |   |
+                |  
+                | 
+                |  
+                |
+                -------
+                """;
+            images[1] =
+                """
+                -----
+                |   |
+                |   0
+                | 
+                |  
+                |
+                -------
+                """;
+            images[2] =
+                """
+                -----
+                |   |
+                |   0
+                |   |
+                |  
+                |
+                -------
+                """;
+            images[3] =
+                """
+                -----
+                |   |
+                |   0
+                |  /|
+                |  
+                |
+                -------
+                """;
+            images[4] =
+                """
+                -----
+                |   |
+                |   0
+                |  /|\
+                |  
+                |
+                -------
+                """;
+            images[5] =
+                """
+                -----
+                |   |
+                |   0
+                |  /|\
+                |  / 
+                |
+                -------
+                """;
+            images[6] =
+                """
+                -----
+                |   |
+                |   0
+                |  /|\
+                |  / \
+                |
+                -------
+                """;
+
+        }
+
     }
 }
+
+
