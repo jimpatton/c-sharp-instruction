126da167390473c2aff17d03c2d7a863fb25f7e7
jimpatton <jimpatton5150@gmail.com>
2025-02-19 17:31:04 -0500

    complete

---
+++ InterestCalculator2/Program.cs
@@ -0,0 +1,30 @@
+﻿using ConsoleLibrary;
+
+namespace InterestCalculator2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyConsole.PrintLine("Interest Calculator2!\n");
+            //While loop: Continue
+            string choice = "y";
+            while (choice == "y")
+            {
+                //prompt for loan amount
+                double loanAmount = MyConsole.PromptDouble("\nEnter loan amount: ");
+                //prompt for interest rate
+                double interestRate = MyConsole.PromptDouble("Enter interest rate: ");
+                //display loan amount and interest rate: doubles
+                MyConsole.PrintLine($"\nLoan amount: \t{loanAmount.ToString("c")}");
+                // interest = loan amount * interest rate
+                MyConsole.PrintLine($"Interest rate: \t{interestRate.ToString("p3")}");
+                MyConsole.PrintLine($"Interest: \t{(loanAmount * interestRate).ToString("c")}");
+
+                choice = MyConsole.PromptString("\nContinue? (y/n) ").ToLower();
+
+            }
+            MyConsole.PrintLine("\nBye");
+        }
+    }
+}
