3acb8ea910f9bf3b0b323eb8cb7de30178a421f5
jimpatton <jimpatton5150@gmail.com>
2025-02-19 19:55:25 -0500

    init commit

---
+++ DiceRoller2/Program.cs
@@ -0,0 +1,49 @@
+﻿using ConsoleLibrary;
+
+namespace DiceRoller2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyConsole.PrintLine("Dice Roller 2\n");
+            string choice = "y";
+            while (choice == "y")
+            {
+
+                Random rand = new Random();
+                int die1 = RollDie(rand);
+                int die2 = RollDie(rand);
+                int total = die1 + die2;
+                MyConsole.PrintLine($"\nDie 1:\t {die1}");
+                MyConsole.PrintLine($"Die 2:\t {die2}");
+                MyConsole.PrintLine($"Total:\t {total}");
+                if (total == 2)
+                {
+                    MyConsole.PrintLine("Snake eyes!");
+                }
+                else if (total == 7)
+                {
+                    MyConsole.PrintLine("Craps!");
+                }
+                else if (total == 12)
+                {
+                    MyConsole.PrintLine("Box cars!");
+                }
+             
+
+
+
+
+                choice = MyConsole.PromptString("\nContinue? (y/n)").ToLower();
+            }
+
+            MyConsole.PrintLine("\nBye");
+        }
+
+        public static int RollDie(Random rand)
+        {
+            return rand.Next(1, 7);
+        }
+    }
+}
