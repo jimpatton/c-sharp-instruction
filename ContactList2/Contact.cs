306c9290a9873911a5337d54b9f512aa9548b071
jimpatton <jimpatton5150@gmail.com>
2025-02-21 18:10:28 -0500

    init commmit

---
+++ ContactList2/Contact.cs
@@ -0,0 +1,31 @@
+﻿namespace ContactList2
+{
+    public class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public Contact() { }
+
+        public Contact(string FirstName, string LastName, string Email, string PhoneNumber)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.Email = Email;
+            this.PhoneNumber = PhoneNumber;
+        }
+                public string DisplayContact() 
+        {
+            string displayStr = "-------------------------------\n";
+            displayStr += "----Current Contact------------\n";
+            displayStr += "-------------------------------\n";
+            displayStr += $"Name:\t\t {FirstName} {LastName}\nEmail Address:\t {Email} \nPhone number:\t {PhoneNumber}";
+            displayStr += "\n-------------------------------\n";
+            return displayStr;
+                        
+        }
+
+    }
+}
---
+++ ContactList2/Program.cs
@@ -0,0 +1,36 @@
+﻿using ConsoleLibrary;
+
+namespace ContactList2
+{
+    internal class Program
+    {
+        static List<Contact> Contacts = new List<Contact>();
+        static void Main(string[] args)
+        {
+            MyConsole.PrintLine("Contact List 2\n");
+            Contact newContact = new Contact();
+            //while loop :continue
+            string choice = "y";
+            while (choice == "y")
+            {
+                //prompt first, last, email, phone: strings
+                string firstName = MyConsole.PromptString("Enter first name: ");
+                string lastName = MyConsole.PromptString("Enter last name: ");
+                string email = MyConsole.PromptString("Enter email: ");
+                string phoneNumber = MyConsole.PromptString("Enter phone number: ");
+                newContact = new Contact(firstName, lastName, email, phoneNumber);
+                Contacts.Add(newContact);
+
+                foreach (Contact contact in Contacts)
+                {
+                    MyConsole.PrintLine(contact.DisplayContact());
+                }
+
+                choice = MyConsole.PromptString("\nContinue? (y/n): ").ToLower();
+            }
+
+
+            MyConsole.PrintLine("\nBye");
+        }
+    }
+}
