ad7736ef89b6987c69c118145334ca3123a3edfd
jimpatton <jimpatton5150@gmail.com>
2025-02-22 14:41:06 -0500

    init commit

---
+++ StudentScores2/Program.cs
@@ -0,0 +1,35 @@
+﻿using ConsoleLibrary;
+
+namespace StudentScores2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyConsole.PrintLine("Student Scores 2\n");
+            List<Student> students = new List<Student>();
+            string choice = "y";
+            while (choice == "y")
+            {
+                MyConsole.PrintLine("\nSTUDENT");
+                string lastName = MyConsole.PromptString("Last name: ");
+                string firstName = MyConsole.PromptString("First name: ");
+                int score = MyConsole.PromptInt("Score: ");
+                Student student = new Student(firstName,lastName,score);
+                students.Add(student);
+
+
+
+                choice = MyConsole.PromptString("\nEnter another student? (y/n): ");
+            }
+
+            MyConsole.PrintLine("\nSUMMARY");
+            foreach (Student student in students)
+            {
+
+                MyConsole.Print(student.ToString());
+            }
+
+        }
+    }
+}
---
+++ StudentScores2/Student.cs
@@ -0,0 +1,21 @@
+﻿namespace StudentScores2
+{
+    internal class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Score { get; set; }
+        
+        public Student(string firstName, string lastName, int score)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Score = score;
+        }
+        public override string ToString()
+        {
+            return ($"\n{LastName}, {FirstName}: {Score}");
+        }
+
+    }
+}
