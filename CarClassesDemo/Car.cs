66605552d8c408d6ced490377771856dd2ec4ecf
jimpatton <jimpatton5150@gmail.com>
2025-03-01 12:31:35 -0500

    init commit

---
+++ CarClassesDemo/Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarClassesDemo
+{
+    internal class Car
+    {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+
+        public Car(int id, string Make, string Model, int Year)
+        {
+            this.Id = id;
+            this.Make = Make;
+            this.Model = Model;
+            this.Year = Year;
+        }
+
+        public string ToString()
+        {
+            return $"Car({Id}: {Make}, {Model}, {Year})";
+        }
+    }
+}
---
+++ CarClassesDemo/Program.cs
@@ -0,0 +1,43 @@
+﻿using ConsoleLibrary;
+
+namespace CarClassesDemo
+{
+    internal class Program
+    {
+        //create list for cars
+        static List<Car> Cars = new List<Car>();
+        static void Main(string[] args)
+        {
+            MyConsole.PrintLine("Car Classes Demo!\n");
+            //create Class for Car
+            //create 3 instances of Car
+            Car newCar = new Car(1,"Jeep", "Wrangler", 2020);
+            //newCar.Id = 1;
+            //newCar.Make = "Jeep";
+            //newCar.Model = "Wrangler";
+            //newCar.Year = 2020;
+            Cars.Add(newCar);
+            string choice = "y";
+            while (choice == "y")
+            {
+                int id = MyConsole.PromptInt("ID: ");
+                string make = MyConsole.PromptString("Make: ");
+                string model = MyConsole.PromptString("Model: ");
+                int year = MyConsole.PromptInt("Year: ");
+                newCar = new Car(id, make, model, year);
+                Cars.Add(newCar);
+
+                choice = MyConsole.PromptString("Add another car? (yes/no): ");
+            }
+            //foreach loop
+            foreach (Car car in Cars)
+            {
+                MyConsole.PrintLine(car.ToString());
+            }
+
+            //print details of cars
+
+            MyConsole.PrintLine("\nBye");
+        }
+    }
+}
