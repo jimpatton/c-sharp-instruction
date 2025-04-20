77193471ce8b4828c932e849f9745eb3b4b57196
jimpatton <jimpatton5150@gmail.com>
2025-04-20 12:58:11 -0400

    rectangle calculator

---
+++ 2-3-RectangleConverter/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _2_3_RectangleConverter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Rectangle Area and Perimeter Calculator");
+            string choice = "y";
+            while (choice.ToLower() == "y")
+            {
+                Console.Write("\nEnter length: ");
+                int l = int.Parse(Console.ReadLine());
+                Console.Write("Enter width: ");
+                int w = int.Parse(Console.ReadLine());
+                //area  = l*w
+                //perimeter = 2*l + 2*w
+                Console.WriteLine($"Area: { l* w}");
+                Console.WriteLine($"Perimeter: {(2 * l) + (2 * w)}");
+
+                Console.Write("\nContinue (y/n): ");
+                choice = Console.ReadLine();
+            }
+                
+            }
+        }
+}
