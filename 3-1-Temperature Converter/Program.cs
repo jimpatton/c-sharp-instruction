7902904cdd4b44a0ed925ea42a2b5cad50a23d84
jimpatton <jimpatton5150@gmail.com>
2025-04-20 13:56:23 -0400

    temp converter

---
+++ 3-1-Temperature Converter/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _3_1_Temperature_Converter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Temperature Converter");
+            string choice = "y".ToLower();
+            while (choice == "y")
+            {
+                Console.Write("Enter the temperature in Fahrenheit: ");
+                float f = float.Parse(Console.ReadLine());
+                // celsius = (farhenheit-32) * 5/9
+                float c = (f - 32) * 5 / 9;
+                Console.WriteLine("Degrees in Celsius: " + c);
+                Console.Write("\nContinue? (y/n): ");
+                choice = Console.ReadLine();
+
+            }
+
+        }
+    }
+}
