fc0b986eb1854284669e2b9a36ec3f6a4d7e4658
jimpatton <jimpatton5150@gmail.com>
2025-04-24 20:19:56 -0400

    Table of Powers

---
+++ 4-1-TableOfPowers/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _4_1_TableOfPowers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Squares and Cubes table");
+            string choice = "y";
+            while (choice == "y".ToLower())
+            {
+                Console.Write("\nEnter and integer: ");
+                int nbr = int.Parse(Console.ReadLine());
+                Console.WriteLine("\nNumber\tSquared\tCubed");
+                Console.WriteLine("======\t=======\t=====");                
+                for (int i = 1; i <= nbr; i++) 
+                Console.WriteLine($"{i}\t\t{i * i}\t\t{i * i * i}");
+                Console.Write("\nContinue? (y/n) ");
+                choice = Console.ReadLine();
+            }
+        }
+    }
+}
