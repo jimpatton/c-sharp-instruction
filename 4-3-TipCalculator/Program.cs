c048f95c38561e3d7a73d0fb8da654e654be553f
jimpatton <jimpatton5150@gmail.com>
2025-04-26 22:11:50 -0400

    tip calculator

---
+++ 4-3-TipCalculator/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _4_3_TipCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Tip Calculator");
+            string choice = "y";
+            while (choice == "y".ToLower()) {
+                Console.Write("\nCost of Meal: ");
+                decimal meal = decimal.Parse(Console.ReadLine());
+                for (decimal d = .15m; d <= .25m; d += .05m)
+                {
+                    decimal tip = meal * d;
+                    decimal total = meal + tip;
+                    Console.WriteLine($"{d:p0}");
+                    Console.WriteLine($"Tip amount: {tip:c}");
+                    Console.WriteLine($"Total amount: {total:c}");
+                }
+                Console.Write("Continue? (y/n):");
+                choice = Console.ReadLine();
+            }
+        }
+    }
+}
