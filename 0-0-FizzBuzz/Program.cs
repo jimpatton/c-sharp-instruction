d4d9781a57d72eef1703e732c3719df7d368d9f0
jimpatton <jimpatton5150@gmail.com>
2025-05-04 20:16:45 -0400

    fizzbuzz validation

---
+++ 0-0-FizzBuzz/Program.cs
@@ -0,0 +1,57 @@
+﻿
+namespace _0_0_FizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fizzbuzz Excercise");
+            string choice = "y";
+            while (choice.ToLower() == "y")
+            {
+                int nbr = GetValidInt("Enter number: ");
+                for (int i = 1; i <= nbr; i++) 
+                {
+                    if (i % 15 == 0)
+                    {
+                        Console.WriteLine("fizzbuzz");
+                    }
+                    else if (i % 5 == 0)
+                    {
+                        Console.WriteLine("buzz");
+                    }
+                    else if (i % 3 == 0)
+                    {
+                        Console.WriteLine("fizz");
+                    }
+                    else 
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+                Console.Write("Continue? (y/n): ");
+                choice = Console.ReadLine();
+
+            }
+        }
+
+        private static int GetValidInt(string prompt)
+        {
+            while (true)
+            {
+                Console.Write(prompt);
+                string userInput = Console.ReadLine();
+                try
+                {
+                    int nbr = int.Parse(userInput);
+                    return nbr;
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine("Error! Please enter a whole number");
+                }
+            }
+        }
+
+    }
+}
